48cd2d1c16758e7ec8c6adf2f732814306ca73ad
Renato Assis <renato.assis@seidor.com.br>
2021-08-11 23:33:49 -0300

    chore: add external file functions

---
+++ utils/functions.sh
@@ -0,0 +1,15 @@
+function docker_deep_clean() {
+    echo "Removing exited containers..."
+    echo "============================="
+    docker ps --filter status=dead --filter status=exited -aq | xargs docker rm -v
+    echo ""
+    echo "Removing unused images..."
+    echo "========================="
+    docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs docker rmi
+    echo ""
+    echo "Removing unused volumes..."
+    echo "=========================="
+    docker volume ls -qf dangling=true | xargs docker volume rm
+    echo ""
+    echo "Done."
+}
