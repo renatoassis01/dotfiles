b9094425507c05cf75145274d1b5d83ac61c6985
Renato Assis <renato.assis@seidor.com.br>
2021-10-19 21:39:14 -0300

    chore: getall parameter

--- utils/functions.sh
+++ utils/functions.sh
@@ -18,11 +18,10 @@ function open-pull-request () {
 
     if [ -z "$1" ]
     then
-      echo "\$title pull request is required"
+      echo "title pull request is required"
     else
-      aws codecommit create-pull-request --title "$1" --targets repositoryName=$(basename `git remote get-url origin`),sourceReference=$(git rev-parse --abbrev-ref HEAD),destinationReference=develop
+      aws codecommit create-pull-request --title "$@" --targets repositoryName=$(basename `git remote get-url origin`),sourceReference=$(git rev-parse --abbrev-ref HEAD),destinationReference=develop
     fi
-
 
 }
 
