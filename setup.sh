ec3636ec3b3631293fdafbd5163edfe38f58b59c
Renato Assis <renato.assis@seidor.com.br>
2021-12-08 22:03:33 -0300

    chore: add pv

--- setup.sh
+++ setup.sh
@@ -3,12 +3,12 @@ set -euo pipefail
 
 # Debian likes
 if [ -x "$(command -v apt)" ]; then
-    sudo apt update && sudo apt install -y curl zsh stow kitty unzip wget xclip make postgresql-client shellcheck pspg miller jq yq
+    sudo apt update && sudo apt install -y curl zsh stow kitty unzip wget xclip make postgresql-client shellcheck pspg miller jq yq pv
 fi
 
 # Arch likes
 if [ -x "$(command -v pacman)" ]; then
-   sudo pacman -S stow zsh kitty unzip wget terminator xclip make shellcheck pspg miller jq yq
+   sudo pacman -S stow zsh kitty unzip wget terminator xclip make shellcheck pspg miller jq yq pv
 fi
 
 clear
