d35d9bceaaed677e34039b47eabc147922af6a83
Renato Assis <renatoweverton@gmail.com>
2023-03-10 13:31:06 -0300

    add nvim setup

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ mkdir -p "$HOME/.config/lazygit"
 
 stow -v  --target="$HOME/.config" config
 stow -v  --dotfiles --target="$HOME" home 
+
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+nvim
